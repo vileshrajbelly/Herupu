2c8e9224aca73dba44ee0ffbafec7e3d3e2580bb
Vilesh Raj Belly <58071849+vileshrajbelly@users.noreply.github.com>
2022-06-21 18:27:32 +0530

    Delete Matlab directory

--- Matlab/dspEXPT2_2.m
+++
@@ -1,9 +0,0 @@
-clear;
-clc;
-num=[1 4];
-den=[1 20 5];
-g=idtf(num,den);
-t=0:0.001:5;
-u=sin(t);
-h=lsim(g,u,t);
-plot(t,h);
--- Matlab/dspEXPT2_3.m
+++
@@ -1,8 +0,0 @@
-clear;
-clc;
-N=input('length of impluse response required: ')
-a=[1,-1,0.9];
-b=[1];
-h=impz(b,a,N);
-stem(h);
-xlabel('N');ylabel('h(n)');title('Impluse response');
