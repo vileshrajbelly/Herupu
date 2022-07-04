09e3620f86e657c548b4feff247f8c0260c86856
Vilesh Raj Belly <58071849+vileshrajbelly@users.noreply.github.com>
2022-07-04 12:37:24 +0530

    Add files via upload

---
+++ Matlab/CS/csEXPT11A.m
@@ -0,0 +1,22 @@
+clc;
+clear;
+num=[1]
+den=[1 2 0 0]
+subplot(3,1,1);
+sys=tf(num,den)
+rlocus(sys)
+title('original system');
+
+num1=[1 1]
+den1=[1 2 0 0]
+subplot(3,1,2);
+sys1=tf(num1,den1)
+rlocus(sys1)
+title('system with additional zero');
+
+num2=[1]
+den2=[1 3 2 0 0]
+subplot(3,1,3);
+sys2=tf(num2,den2)
+rlocus(sys2)
+title('system with additional pole');
