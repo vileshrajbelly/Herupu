136c5f7ed42d2658a6df11171e894ec2416196f4
Vilesh Raj Belly <58071849+vileshrajbelly@users.noreply.github.com>
2022-07-05 18:22:15 +0530

    Update dspEXPT2_3.m

--- Matlab/DSP/dspEXPT2_3.m
+++ Matlab/DSP/dspEXPT2_3.m
@@ -1,8 +1,8 @@
 clear;
 clc;
 N=input('length of impluse response required: ')
-a=[1,-1,0.9];
-b=[1];
-h=impz(b,a,N);
+a=[1];
+b=[1,-1,0.9];
+h=impz(a,b,N);
 stem(h);
 xlabel('N');ylabel('h(n)');title('Impluse response');
