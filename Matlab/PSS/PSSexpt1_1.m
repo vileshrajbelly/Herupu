18891a1016aabfddf5e0b42ee3683012910f453d
Vilesh Raj Belly <58071849+vileshrajbelly@users.noreply.github.com>
2022-10-13 21:38:45 +0530

    Add files via upload

---
+++ Matlab/PSS/PSSexpt1_1.m
@@ -0,0 +1,26 @@
+clc;
+clear;
+l=40;
+z=0.2+0.408i;
+y=0+3.14e-6i;
+vr=132e3;
+ir=174.96-131.22i;
+Z=z*l;
+Y=y*l;
+A=1;
+D=A;
+B=Z;
+C=0;
+fprintf("A*D-B*C= %0.0f\n", A*D-B*C);
+
+vrph=vr/sqrt(3);
+vsph=A*vrph+B*ir;
+is=C*vrph+D*ir;
+pr=3*real(vrph*conj(ir))/1e6;
+ps=3*real(vsph*conj(is))/1e6;
+
+eff=100*pr/ps;
+fprintf("effiency= %0.3f\n", eff);
+
+vreg=real(vsph-vrph)/vrph*100;
+fprintf("voltage regulation= %0.3f\n", vreg);
